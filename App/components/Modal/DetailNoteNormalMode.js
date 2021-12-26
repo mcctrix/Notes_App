06241aeffe6cc1875f66930208fda4fa3a53535f
ctrix <csk77@pm.me>
2021-12-26 20:37:22 +0530

    Added Fav Icon

--- App/components/Modal/DetailNoteNormalMode.js
+++ App/components/Modal/DetailNoteNormalMode.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
 import GlobalStyles from "../constants/GlobalStyles";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function DetailNoteNormalMode(props) {
+  const Fav = (
+    <>
+      <Text style={[styles.FavStyle, GlobalStyles.Font]}>Fav:</Text>
+      <MaterialIcons
+        name="favorite"
+        size={45}
+        color="red"
+        style={styles.StyleFav}
+      />
+    </>
+  );
   return (
     <>
       <TouchableOpacity
@@ -14,9 +26,7 @@ export default function DetailNoteNormalMode(props) {
       <Text style={[styles.TitleStyle, GlobalStyles.Font]}>
         {props.Data.Data.note.value}
       </Text>
-      {props.Data.Data.note.isFav == true && (
-        <Text style={[styles.FavStyle, GlobalStyles.Font]}>Favourite Note</Text>
-      )}
+      {props.Data.Data.note.isFav == true && Fav}
       <Text style={[{ fontSize: 30 }, GlobalStyles.Font]}>
         {props.Data.Data.note.Des}
       </Text>
@@ -46,6 +56,14 @@ const styles = StyleSheet.create({
   },
   FavStyle: {
     fontSize: 33,
-    color: "tomato",
+    color: "red",
+    position: "absolute",
+    top: 20,
+    left: 110,
+  },
+  StyleFav: {
+    position: "absolute",
+    top: 20,
+    alignSelf: "center",
   },
 });
